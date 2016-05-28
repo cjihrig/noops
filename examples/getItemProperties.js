bcbfd1f3343fcf15bee344b4b557d92129da0d17
cjihrig <cjihrig@gmail.com>
2016-05-28 15:24:58 -0400

    support request validation with joi

--- examples/getItemProperties.js
+++ examples/getItemProperties.js
@@ -1,7 +1,19 @@
 'use strict';
 
+const Joi = require('joi');
+
+
 module.exports = {
   id: 'getItemProperties',
+  validate: {
+    params: {
+      itemId: Joi.string().required().description('The item ID to retrieve')
+    },
+    query: Joi.object().keys({
+      bar: Joi.string(),
+      baz: Joi.string()
+    }).xor('bar', 'baz')
+  },
   handler: function (request, reply) {
     reply(null, {
       summary: `props for item ${request.params.itemId}`,
